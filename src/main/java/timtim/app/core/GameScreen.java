04f00044f48bb5b5e2eb3b19dd6d6cee41537b41
xat013 <xat013@uib.no>
2023-02-22 17:35:41 +0100

    attempted separating into MVC

--- src/main/java/timtim/app/core/GameScreen.java
+++ src/main/java/timtim/app/core/GameScreen.java
@@ -13,84 +13,69 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
+import timtim.app.controller.GameController;
+import timtim.app.controller.IGameController;
 import timtim.app.manager.Const;
 import timtim.app.manager.TileMapManager;
+import timtim.app.model.GameModel;
+import timtim.app.model.IGameModel;
 import timtim.app.objects.Player;
 
-public class GameView extends ScreenAdapter {
+public class GameScreen extends ScreenAdapter {
 
+	IGameModel model;
+	IGameController controller;
+	
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private World world;
 	private Box2DDebugRenderer B2DDebugRenderer;
 	
 	private OrthogonalTiledMapRenderer mapRenderer;
-	private TileMapManager mapManager;
 	
-	// objects
-	Player player;
-	
-	public GameView(OrthographicCamera camera) {
+	public GameScreen(OrthographicCamera camera) {
+		this.model = new GameModel(this);
+		this.controller = new GameController(this);
 		this.camera = camera;
 		this.batch = new SpriteBatch();
-		this.world = new World(new Vector2(0,-9.81f), false);
+
 		this.B2DDebugRenderer = new Box2DDebugRenderer();
 		
 		// MAP INIT
-		this.mapManager = new TileMapManager(this);
-		this.mapRenderer = mapManager.mapSetup();
+		this.mapRenderer = this.model.getMapRenderer();
 	}
 	
 	@Override
 	public void render(float delta) {
-		this.update();
+		this.controller.update();
+		updateCamera();
 		
 		// Removes all graphics and animations from last frame
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		// Render map
+		mapRenderer.setView(camera);
 		mapRenderer.render();
 		
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		//render objects
 		
 		batch.end();
-		B2DDebugRenderer.render(world, camera.combined.scl(Const.PPM));
-	}
-
-	
-	private void update() {
-		world.step(1/60f, 6, 2);
-		updateCamera();
-		player.update();
-		
-		batch.setProjectionMatrix(camera.combined);
-		mapRenderer.setView(camera);
-		
-		if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) { //  Closes game if escape is pressed
-			Gdx.app.exit();
-		}
+		B2DDebugRenderer.render(model.getWorld(), camera.combined.scl(Const.PPM));
 	}
 
 	private void updateCamera() {
 		Vector3 pos = camera.position;
 		// takes player position and multiply by PPM for real world position, 
 		// 		multiply by 10 and divide by 10 for smoother camera movement
-		pos.x = Math.round(player.getBody().getPosition().x * Const.PPM * 10) / 10f;
-		pos.y = Math.round(player.getBody().getPosition().y * Const.PPM * 10) / 10f;
+		pos.x = Math.round(model.getPlayer().getBody().getPosition().x * Const.PPM * 10) / 10f;
+		pos.y = Math.round(model.getPlayer().getBody().getPosition().y * Const.PPM * 10) / 10f;
 		camera.position.set(pos);
 		camera.update();
 	}
-
-	/**
-	 * @return the world
-	 */
-	public World getWorld() {
-		return world;
-	}
 	
-	public void setPlayer(Player p) {
-		this.player = p;
+	public IGameModel getModel() {
+		return this.model;
 	}
 }
