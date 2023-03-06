bacf0623fb62bcdf31d1bdf56648a7b110e7d507
Sandra <bib016@uib.no>
2023-03-06 23:06:41 +0100

    Done some work in inventory class, plus added an item class

--- src/main/java/timtim/app/objects/Player/inventory.java
+++ src/main/java/timtim/app/objects/Player/inventory.java
@@ -1,7 +1,36 @@
 package timtim.app.objects.Player;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 public class inventory {
 
-    
+    int max_size = 5;
+    ArrayList<Items> items = new ArrayList <Items>();
+
+    public int inventoryMaxSize(){
+        return max_size;
+    }
+
+    public int nItemsInInventory(){
+        return items.size();
+    }
+
+    public boolean placeInInventory(Items item){
+        if (items.size()>= inventoryMaxSize()){
+            return false;
+        }
+        else 
+            items.add(item);
+            return true; 
+    }
+
+    public void takeOutItem(Items item){
+        if(items.contains(item)){
+            items.remove(item);
+        }
+        else 
+        throw new NoSuchElementException("Inventory does not contain this item");
+    }
     
 }
