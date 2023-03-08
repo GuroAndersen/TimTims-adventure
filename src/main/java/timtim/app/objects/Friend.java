6100e90b1fa510f398f66e854d8fce189c74949c
Guro <queengurp@gmail.com>
2023-03-08 19:11:28 +0100

    Friend will only repeat the last two chat options when it has already been interacted with before

--- src/main/java/timtim/app/objects/Friend.java
+++ src/main/java/timtim/app/objects/Friend.java
@@ -1,50 +1,103 @@
 package timtim.app.objects;
 
-public class Friend implements IFriend {
-    
-    private String[] dialogueOptions = {"Hei", "Kan du hjelpe meg?", "Jeg trenger en jakke", "Se etter kister"};
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Friend extends GameEntity implements IFriend {
+
+    private String[] dialogueOptions = { "Hei", "Kan du hjelpe meg?", "Jeg trenger en jakke", "Se etter kister" };
     private int dialogueCounter = 0;
-    private String[] giftDialogueOptions = {"Takk for jakken!", "Takk for medaljongen", "Takk for boken"};
+    private String[] giftDialogueOptions = { "Takk for jakken!", "Takk for medaljongen", "Takk for boken" };
+    private int interactionCounter = 0;
 
     /**
-     * getConversation finds a string to show when a conversation with a Friend has been initiated.
+     * getConversation finds a string to show when a conversation with a Friend has
+     * been initiated.
      * Gets it from a list of Strings.
      */
     @Override
     public String getConversation() {
-        
-        
-        for (String dialogue : dialogueOptions) {
-            if (isEKeyPressed) {
-                String currentDialogue = dialogueOptions[dialogueCounter];
-                dialogueCounter++;
-                if (dialogueCounter >= dialogueOptions.length) {
-                    dialogueCounter = 0;
-                }
-                return currentDialogue;
-            } 
-            else {
+        // First interaction between Player and Friend
+        // Will go through the whole list of conversation options
+        if (interactionCounter == 0) {
+            String currentDialogue = dialogueOptions[dialogueCounter];
+            dialogueCounter++;
+            if (dialogueCounter >= dialogueOptions.length) {
+                dialogueCounter = 0;
                 return null;
             }
+            return currentDialogue;
         }
+
+        // Second or more interaction between Player and Friend
+        // Will only show the relevant parts of dialogue needed
+        if (interactionCounter > 0) {
+            int relevantDialogueCounter = 3;
+            String relevantDialogue = dialogueOptions[relevantDialogueCounter];
+            relevantDialogueCounter++;
+            if (relevantDialogueCounter >= dialogueOptions.length) {
+                relevantDialogueCounter = 3;
+                return null;
+            }
+            return relevantDialogue;
+        }
+
+        interactionCounter++;
+        return null;
     }
 
     @Override
     public boolean hasRecievedGift() {
-        
-        if (isFKeyPressed) {
-            return true;
-        }
-        return false;
+        // If the player has the item in his inventory then by pressing e while close to
+        // Friend will return true.
+        // If the player does not have the item it will return false.
+
+        /**
+         * If (player has item) {
+         * return true;
+         * }
+         * else {
+         * return false;
+         * }
+         * 
+         * 
+         */
+        return true;
+    }
+
+    public String giftToNpc() {
+
+        if (hasRecievedGift() == true)
+            return giftDialogueOptions[0];
+
+        return null;
     }
 
     @Override
-    public String giftToNpc() {
-       
-        
-        if (hasRecievedGift == true)
-            return giftDialogueOptions[0];
-        
-        return null;
+    public void move() {
+        // TODO Auto-generated method stub
+        // This method is supposed to be a restricted move method where the Friend
+        // object can only move a certain way.
+        // A starter coordinate and then move x ammount of steps in left and right
+        // direction.
+
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+
+    @Override
+    public String giftToNpcDialogue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'giftToNpcDialogue'");
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
     }
 }
--- src/test/java/timtim/app/objects/FriendTest.java
+++ src/test/java/timtim/app/objects/FriendTest.java
@@ -1,38 +1,46 @@
 package timtim.app.objects;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import timtim.app.objects.Friend;
 
 public class FriendTest {
-    
+
     Friend friend = new Friend();
 
     @Test
     public void testMove() {
-        
+
     }
 
     @Test
     public void testGiftToNPC() {
-        //Test the different gifts on the different levels.
-        assertEquals("Thanks for the jacket!", friend.giftToNpc("jacket"));
-        assertEquals("Thanks for the medallion!", friend.giftToNpc("medallion"));
-        assertEquals("Thanks for the book!", friend.giftToNpc("book"));
+        // Test the different gifts on the different levels.
+        assertEquals("Takk for jakken!", friend.giftToNpc());
+        // assertEquals("Takk for medaljongen!", friend.giftToNpc());
+        // assertEquals("Takk for boken!", friend.giftToNpc());
     }
 
     @Test
     public void testGiftToNpcDoesNotModifyState() {
         // Test that the method does not modify any internal state
-        String gift = "jacket";
-        String response = friend.giftToNpc(gift);
-        assertEquals("Thanks for the jacket!", response);
-        // Verify that the giftToNpc method did not modify the gift or any other internal state
-        assertEquals("jacket", gift);
+        String gift = "jakke";
+        String response = friend.giftToNpc();
+        assertEquals("Takk for jakken!", response);
+        // Verify that the giftToNpc method did not modify the gift or any other
+        // internal state
+        assertEquals("jakke", gift);
     }
 
     @Test
     public void testGetConversation() {
-        //Tests if the method returns the correct dialogue at the correct time.
+        // Tests if the method returns the correct dialogue at the correct time.
         assertEquals("Hei", friend.getConversation());
+        assertEquals("Kan du hjelpe meg?", friend.getConversation());
+        assertEquals("Jeg trenger en jakke", friend.getConversation());
+        assertEquals("Se etter kister", friend.getConversation());
+        assertEquals(null, friend.getConversation());
     }
 }
