e1289ca8189f3889f04c212bf8ee2c3c23c0cd4a
Guro Andersen <gan019@uib.no>
2023-03-20 13:57:21 +0100

    Started implementing beginContact, but needs more to be complete

--- src/main/java/timtim/app/core/MyContactListener.java
+++ src/main/java/timtim/app/core/MyContactListener.java
@@ -6,5 +6,29 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 public class MyContactListener implements ContactListener {
-    
+
+    // Gets activated when two objects make contact with eachother.
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture fa = contact.getFixtureA();
+        Fixture fb = contact.getFixtureB();
+
+        
+    }
+
+    // Gets activated when two objects stop having contact with eachother.
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
 }
