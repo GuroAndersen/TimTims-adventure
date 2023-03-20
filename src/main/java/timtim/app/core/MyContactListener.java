00cba59e82c2e999d4ad024026c9637d18b43d26
Guro Andersen <gan019@uib.no>
2023-03-20 12:28:26 +0100

    MyContactListener is set up and ready to start implementing

---
+++ src/main/java/timtim/app/core/MyContactListener.java
@@ -0,0 +1,10 @@
+package timtim.app.core;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class MyContactListener implements ContactListener {
+    
+}
