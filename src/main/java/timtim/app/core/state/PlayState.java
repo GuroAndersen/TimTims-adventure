ecf7dbb83af8fc961da233dd1cc9d2d0a7fcd1d6
Hilde Jordal <xat013@uib.no>
2023-03-30 12:26:48 +0200

    added wood tsx back in

--- src/main/java/timtim/app/core/state/PlayState.java
+++ src/main/java/timtim/app/core/state/PlayState.java
@@ -39,7 +39,6 @@ public class PlayState implements StateHandler {
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) { // Closes game if escape is pressed
             Gdx.app.exit();
         }
-
         // Horizontal movement
         boolean moveLeft = false;
         boolean moveRight = false;
