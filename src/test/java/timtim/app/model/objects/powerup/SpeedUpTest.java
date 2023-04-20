b83ac6f45e11cea543c1f17d769fd0d674d8c8fa
Mattias <nus019@uib.no>
2023-04-20 18:29:56 +0200

    started creating tests for PowerUp

---
+++ src/test/java/timtim/app/model/objects/powerup/SpeedUpTest.java
@@ -0,0 +1,33 @@
+package timtim.app.model.objects.powerup;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import timtim.app.model.objects.Player;
+
+public class SpeedUpTest {
+
+    private Player player;
+    private SpeedUp speedUp;
+
+    @BeforeEach
+    public void setUp() {
+        player = new Player(100, 10);
+        speedUp = new SpeedUp();
+    }
+
+    @Test
+    public void testSpeedUpEffect() {
+        float initialSpeed = player.getSpeed();
+        speedUp.doPowerupIfActive(player);
+        float newSpeed = player.getSpeed();
+
+        assertEquals(initialSpeed + 2, newSpeed, 0.001);
+    }
+
+    // Needs this in the player class:
+    // public float getSpeed() {
+	// 	return speed;
+	// }
+}
